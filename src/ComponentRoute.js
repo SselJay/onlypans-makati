cc46b41ca71e677a964993646e23dcb99f6ae43d
SselJay <russeljaybuan@gmail.com>
2022-11-11 15:27:57 +0800

    update

--- src/ComponentRoute.js
+++ src/ComponentRoute.js
@@ -13,7 +13,7 @@ import UserMenu from './usercomponents/UserMenu';
 const Componentroute = () => {
     return (
         <Routes>
-            <Route path='/' element={<Home/>}/>
+            <Route index element={<Home/>}/>
             <Route path='/home' element={<Home/>}/>
             <Route path='/add' element={<Adduser/>}/>
             <Route path='/admin' element={<Allusers/>}/>
