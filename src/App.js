6dc3a7f237316a4c92e2beee27566c6231971c07
SselJay <russeljaybuan@gmail.com>
2022-11-12 10:34:09 +0800

    hello

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Componentroute from "./ComponentRoute";
-import{ BrowserRouter} from "react-router-dom";
+import{ HashRouter} from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 function App() {
   return (
-      <BrowserRouter>
+      <HashRouter>
         <Componentroute/>
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
