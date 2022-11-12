9eebca4ae8b9175eba68206f4b163bbb263fa39e
SselJay <russeljaybuan@gmail.com>
2022-11-12 10:34:50 +0800

    hello

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/onlypans-makati/precache-manifest.bc26cd825d5681d03a7981a72c0d7c4a.js"
+  "/onlypans-makati/precache-manifest.a88096b61e2669975e1a8ece936f291f.js"
 );
 
 workbox.clientsClaim();
